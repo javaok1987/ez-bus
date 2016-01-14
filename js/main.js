03aba8501664760df8597e27b5549000dab703cb
ianliao <ianliao@eztravel.com.tw>
2016-01-14 13:22:27 +0800

    Update 2016-01-14T05:22:24.092Z

--- js/main.js
+++ js/main.js
@@ -116,94 +116,95 @@ if ( typeof define === 'function' && define.amd ) {
 
 // Extend JS String with repeat method
 String.prototype.repeat = function(num) {
-    return new Array(Math.round(num) + 1).join(this);
+  return new Array(Math.round(num) + 1).join(this);
 };
 
 (function($) {
 
-    // Add segments to a slider
-    $.fn.addSliderSegments = function() {
-        return this.each(function() {
-            var $this = $(this),
-                option = $this.slider('option'),
-                amount = (option.max - option.min) / option.step,
-                orientation = option.orientation;
-            if ('vertical' === orientation) {
-                var output = '',
-                    i;
-                console.log(amount);
-                for (i = 1; i <= amount - 1; i + 1) {
-                    output += '<div class="ui-slider-segment" style="top:' + 100 / amount * i + '%;"></div>';
-                }
-                $this.prepend(output);
-            } else {
-                var segmentGap = 100 / (amount) + '%';
-                var segment = '<div class="ui-slider-segment" style="margin-left: ' + segmentGap + ';"></div>';
-                $this.prepend(segment.repeat(amount - 1));
-            }
-        });
-    };
-
-    jQuery(function($) {
-
-        var $slider = $('#slider'),
-            $walkSlider = $('#walk-slider'),
-            sliderValueMultiplier = 15;
-
-        // Disable link clicks to prevent page scrolling
-        $(document).on('click', 'a[href="#fakelink"]', function(e) {
-            e.preventDefault();
-        });
-
-        // Custom Selects
-        if ($('[data-toggle="select"]').length) {
-            $('[data-toggle="select"]').select2(); 
-            $('[data-toggle="select"]').keydown(false);;
+  // Add segments to a slider
+  $.fn.addSliderSegments = function() {
+    return this.each(function() {
+      var $this = $(this),
+        option = $this.slider('option'),
+        amount = (option.max - option.min) / option.step,
+        orientation = option.orientation;
+      if ('vertical' === orientation) {
+        var output = '',
+          i;
+        console.log(amount);
+        for (i = 1; i <= amount - 1; i + 1) {
+          output += '<div class="ui-slider-segment" style="top:' + 100 / amount * i + '%;"></div>';
         }
-
-        // jQuery UI Sliders
-        if ($slider.length > 0) {
-            $slider.slider({
-                min: 1,
-                max: 4,
-                value: 2,
-                orientation: 'horizontal',
-                range: 'min',
-                slide: function(event, ui) {
-                    var _value = ui.value * sliderValueMultiplier;
-                    $slider.find('.ui-slider-value:last').text(_value + ' 分鐘').data('slidervalue', _value);
-                }
-            }).addSliderSegments($slider.slider('option').max);
-        }
-
-        $('.btn-group').on('click', 'a', function() {
-            $(this).siblings().removeClass('active').end().addClass('active');
-        });
-
-        if ($walkSlider.length > 0) {
-            $walkSlider.slider({
-                min: 1,
-                max: 4,
-                value: 2,
-                orientation: 'horizontal',
-                range: 'min',
-                slide: function(event, ui) {
-                    var _value = ui.value * sliderValueMultiplier;
-                    $walkSlider.find('.ui-slider-value:last').text(_value + ' 分鐘').data('slidervalue', _value);
-                }
-            }).addSliderSegments($walkSlider.slider('option').max);
-        }
-
-        // Checkboxes and Radio buttons
-        $('[data-toggle="checkbox"]').radiocheck();
-        $('[data-toggle="radio"]').radiocheck();
-
-        // Switches
-        if ($('[data-toggle="switch"]').length) {
-            $('[data-toggle="switch"]').bootstrapSwitch();
-        }
-
+        $this.prepend(output);
+      } else {
+        var segmentGap = 100 / (amount) + '%';
+        var segment = '<div class="ui-slider-segment" style="margin-left: ' + segmentGap + ';"></div>';
+        $this.prepend(segment.repeat(amount - 1));
+      }
     });
+  };
+
+  jQuery(function($) {
+
+    var $slider = $('#slider'),
+      $walkSlider = $('#walk-slider'),
+      sliderValueMultiplier = 15;
+
+    // Disable link clicks to prevent page scrolling
+    $(document).on('click', 'a[href="#fakelink"]', function(e) {
+      e.preventDefault();
+    });
+
+    // Custom Selects
+    if ($('[data-toggle="select"]').length) {
+      $('[data-toggle="select"]').select2({
+        minimumResultsForSearch: -1
+      });
+    }
+
+    // jQuery UI Sliders
+    if ($slider.length > 0) {
+      $slider.slider({
+        min: 1,
+        max: 4,
+        value: 2,
+        orientation: 'horizontal',
+        range: 'min',
+        slide: function(event, ui) {
+          var _value = ui.value * sliderValueMultiplier;
+          $slider.find('.ui-slider-value:last').text(_value + ' 分鐘').data('slidervalue', _value);
+        }
+      }).addSliderSegments($slider.slider('option').max);
+    }
+
+    $('.btn-group').on('click', 'a', function() {
+      $(this).siblings().removeClass('active').end().addClass('active');
+    });
+
+    if ($walkSlider.length > 0) {
+      $walkSlider.slider({
+        min: 1,
+        max: 4,
+        value: 2,
+        orientation: 'horizontal',
+        range: 'min',
+        slide: function(event, ui) {
+          var _value = ui.value * sliderValueMultiplier;
+          $walkSlider.find('.ui-slider-value:last').text(_value + ' 分鐘').data('slidervalue', _value);
+        }
+      }).addSliderSegments($walkSlider.slider('option').max);
+    }
+
+    // Checkboxes and Radio buttons
+    $('[data-toggle="checkbox"]').radiocheck();
+    $('[data-toggle="radio"]').radiocheck();
+
+    // Switches
+    if ($('[data-toggle="switch"]').length) {
+      $('[data-toggle="switch"]').bootstrapSwitch();
+    }
+
+  });
 
 })(jQuery);
 
